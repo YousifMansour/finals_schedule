e42d351e77b0c9f1748d651dabdc35534b2365cd
Yousif <yousifyacoub1997@gmail.com>
2018-02-03 13:36:08 +0200

    added https and reverse proxy for node server

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,19 +7,19 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getFinal(courseName: string, courseNumber: string) {
-    var url: string = 'http://45.32.150.44:8080/api/:' + courseName +
+    var url: string = 'https://aub-finals.tk/api/:' + courseName +
         '/:' + courseNumber + '/getFinal';
     return this.http.get(url, {responseType: 'text'});
   }
 
   getFinalBySection(courseName: string, courseNumber: string, section: string) {
-    var url: string = 'http://45.32.150.44:8080/api/:' + courseName +
+    var url: string = 'https://aub-finals.tk/api/:' + courseName +
         '/:' + courseNumber + '/:' + section + '/getFinalBySection';
     return this.http.get(url, {responseType: 'text'});
   }
 
   getAllFinals() {
-    var url = 'http://45.32.150.44:8080/api/getData';
+    var url = 'https://aub-finals.tk/api/getData';
     return this.http.get(url, {responseType: 'text'});
   }
 }
